f5257aa0fe81428eacaa1be012285335b0eb52a0
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2025-01-31 20:24:07 -0500

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,14 @@
+namespace DrMonkeyStreamsStuff2000ftUnderground;
+
+public static class ModHelperData
+{
+    public const string WorksOnVersion = "46.0";
+    public const string Version = "1.1.0";
+    public const string Name = "Dr Monkey Streams Stuff 2000ft Underground";
+
+    public const string Description = "Dr Monkey Streams Stuff 2000ft Underground. Also theres a boss I guess";
+    public const string RepoOwner = "DatJaneDoe";
+    public const string RepoName = "UnityModJamProject";
+
+	
+}
