d037df088e86628428d324650e27791348d79fa6
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2025-02-05 02:44:32 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,11 +2,11 @@ namespace DrMonkeyStreamsStuff2000ftUnderground;
 
 public static class ModHelperData
 {
-    public const string WorksOnVersion = "46.0";
-    public const string Version = "1.1.0";
-    public const string Name = "Dr Monkey Streams Stuff 2000ft Underground";
+    public const string WorksOnVersion = "47.0";
+    public const string Version = "1.1.1";
+    public const string Name = "Dr Monkey Gaming Vs The Bloon God";
 
-    public const string Description = "Dr Monkey Streams Stuff 2000ft Underground. Also theres a boss I guess";
+    public const string Description = "Dr Monkey Streams 2000ft Underground while you fight the Bloon God";
     public const string RepoOwner = "DatJaneDoe";
     public const string RepoName = "UnityModJamProject";
 
