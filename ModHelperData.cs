7a01a43b7e125d2c12468d572d279617d6cfdd7b
DatJaneDoe <54993349+DatJaneDoe@users.noreply.github.com>
2025-02-05 02:49:43 -0500

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,9 +4,9 @@ public static class ModHelperData
 {
     public const string WorksOnVersion = "47.0";
     public const string Version = "1.1.1";
-    public const string Name = "Dr Monkey Gaming Vs The Bloon God";
+    public const string Name = "Vs The Bloon God";
 
-    public const string Description = "Dr Monkey Streams 2000ft Underground while you fight the Bloon God";
+    public const string Description = "Boss fight against the Bloon God. Also Dr Monkey is confirmed epic gamer";
     public const string RepoOwner = "DatJaneDoe";
     public const string RepoName = "UnityModJamProject";
 
